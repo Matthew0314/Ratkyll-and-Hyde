c73edfd3ddeddef14b0e61fb48c5ff15f7b43289
Matthew0314 <matthewh314@gmail.com>
2025-02-18 16:31:36 -0500

    Organized Files

---
+++ RatKyll and Hide/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,28 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerController : MonoBehaviour
+{
+
+    [SerializeField] float speed = 5; // Speed of character
+    [SerializeField] int playerNum; // Either 0 for Player1 or 1 for Player2, set in the inspector
+    private Vector2 movementInput;
+    private PlayerInput playerInput;
+    private Gamepad playerGamepad;
+
+    void Start() {
+      
+        //Assigns a controller to the player
+        playerInput = GetComponent<PlayerInput>();
+        playerInput.SwitchCurrentControlScheme(Gamepad.all[playerNum]);
+        playerGamepad = Gamepad.all[playerNum];
+    }
+    void Update()
+    {
+        // Moves the Player
+        transform.Translate(new Vector3(movementInput.x, 0, movementInput.y) * speed * Time.deltaTime);
+    }
+
+    // Moves the character using Unity's Input System
+    public void OnMove(InputAction.CallbackContext ctx) => movementInput = ctx.ReadValue<Vector2>();
+}
