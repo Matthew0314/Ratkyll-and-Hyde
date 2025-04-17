6a4c7a6d0771e86d4aae70ca7a8d2e60dae56a7a
Matthew0314 <matthewh314@gmail.com>
2025-04-17 18:59:28 -0400

    Began working on spawning food randomly throughout the scene

---
+++ RatKyll and Hide/Assets/Scripts/FoodSpawnManager.cs
@@ -0,0 +1,50 @@
+using UnityEngine;
+using System.Collections.Generic;
+
+public class FoodSpawnManager : MonoBehaviour
+{
+    [Header("Spawn Settings")]
+    public List<Transform> spawnPoints;        // Invisible GameObjects defining positions
+    public List<GameObject> foodPrefabs;       // Food prefabs to randomly choose from
+
+    private Dictionary<Transform, GameObject> activeFood = new Dictionary<Transform, GameObject>();
+
+    void Start()
+    {
+        int spawnCount = spawnPoints.Count / 2;
+        for (int i = 0; i < spawnCount; i++)
+        {
+            SpawnFoodAtRandomFreePoint();
+        }
+    }
+
+    public void SpawnFoodAtRandomFreePoint()
+    {
+        // Gets list of all avaliable points
+        List<Transform> availablePoints = spawnPoints.FindAll(p => !activeFood.ContainsKey(p));
+        if (availablePoints.Count == 0) return;
+
+        // Chooses a random point and a random prefab
+        Transform chosenPoint = availablePoints[Random.Range(0, availablePoints.Count)];
+        GameObject prefab = foodPrefabs[Random.Range(0, foodPrefabs.Count)];
+
+
+        GameObject spawned = Instantiate(prefab, chosenPoint.position, Quaternion.identity);
+        activeFood[chosenPoint] = spawned;
+
+        // Let the food know who its manager is
+        // if (spawned.TryGetComponent<FoodItem>(out var foodItem))
+        // {
+        //     foodItem.SetManager(this, chosenPoint);
+        // }
+    }
+
+    public void NotifyFoodDestroyed(Transform spawnPoint)
+    {
+        if (activeFood.ContainsKey(spawnPoint))
+        {
+            activeFood.Remove(spawnPoint);
+            SpawnFoodAtRandomFreePoint();
+        }
+    }
+}
